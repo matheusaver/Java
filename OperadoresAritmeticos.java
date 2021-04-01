0fb89a8c3f752541db96d6ddb1c64f92da8e140c
matheusaver <80966225+matheusaver@users.noreply.github.com>
2021-04-01 14:52:49 -0300

    Add files via upload

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,24 @@
+package net.weg.cursojava.aula13;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+
+		int resultado = 1 + 2;
+		System.out.println(resultado);
+		
+		resultado = resultado - 1;
+		System.out.println(resultado);
+
+		resultado = resultado * 3;
+		System.out.println(resultado);
+		
+		resultado = resultado / 2;
+		System.out.println(resultado);
+		
+		resultado = resultado % 2;
+		System.out.println(resultado);
+		
+	}
+
+}
